1cd900c7b00a623b42436205713e4802ba7d1d44
Manthan B Y <manthan.gowda@gmail.com>
2018-10-04 20:59:24 +0530

    Added Working nav bar which is responsive.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-seconds = 5
+seconds = 5;
 function redirect(event){
  if (seconds <=0){
  // redirect to new url after counter  down.
   window.open("res_login.html","_self");
  }else{
   seconds--;
-  document.getElementById("pageInfo").innerHTML = "Form Successfully Submitted. Redirecting to login page in "+seconds+" seconds.Please wait..."
-  setTimeout("redirect()", 1000)
+  document.getElementById("pageInfo").innerHTML = "Form Successfully Submitted. Redirecting to login page in "+seconds+" seconds.Please wait...";
+  setTimeout("redirect()", 1000);
  }
  event.stopPropagation();
 }
